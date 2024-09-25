d068e1b7ff361cac29816b0a3d33aff9c4c21dd1
Muhammad Saqib <sm4179515@gmail.com>
2024-09-25 15:58:28 +0500

    Set up server to listen on defined port

--- express-backend/server.mjs
+++ express-backend/server.mjs
@@ -3,3 +3,8 @@ import express from 'express';
 const PORT = 3000;
 const app = express();
 app.use(express.json());
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
